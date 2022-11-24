e9742339f65489bb88bceebb3e696683fdf236b6
MOHAMED Ibrahim <mohamed.ibm@hotmail.fr>
2022-11-24 16:08:58 +0100

    ajout layout

---
+++ lokidor.sql
@@ -0,0 +1,27 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1deb2ubuntu2
+-- http://www.phpmyadmin.net
+--
+-- Client :  localhost
+-- Généré le :  Jeu 26 Octobre 2017 à 13:53
+-- Version du serveur :  5.7.19-0ubuntu0.16.04.1
+-- Version de PHP :  7.0.22-0ubuntu0.16.04.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `lokidor`
+--
+
+-- --------------------------------------------------------​
+
+DROP DATABASE IF EXISTS lokidor;
+CREATE DATABASE lokidor;
+USE lokidor;
