0a9e1899c233341fce780e7af84fae64802edce9
Jottundr <bnjhmr@gmail.com>
2022-11-24 15:58:30 +0100

    view for destination OK

---
+++ lokidor.sql
@@ -0,0 +1,63 @@
+DROP DATABASE IF EXISTS lokidor;
+
+CREATE DATABASE lokidor;
+
+USE lokidor;
+
+CREATE TABLE destination (
+  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  type VARCHAR(255) NOT NULL,
+  distance FLOAT NOT NULL,
+  min_temperature INT NOT NULL,
+  max_temperature INT NOT NULL,
+  image VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE shuttle (
+  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  type VARCHAR(255) NOT NULL,
+  seats INT NOT NULL,
+  speed FLOAT NOT NULL
+);
+
+CREATE TABLE expedition (
+  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  date DATE NOT NULL,
+  time INT NOT NULL,
+  price INT NOT NULL,
+  shuttle_id INT NOT NULL,
+  destination_id INT NOT NULL,
+  CONSTRAINT fk_expedition_shuttle
+    FOREIGN KEY (shuttle_id)
+    REFERENCES shuttle(id),
+  CONSTRAINT fk_expedition_destination
+    FOREIGN KEY (destination_id)
+    REFERENCES destination(id)
+);
+
+CREATE TABLE user (
+  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  firstname VARCHAR(255) NOT NULL,
+  lastname VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  expedition_id INT NOT NULL,
+  CONSTRAINT fk_user_expedition
+    FOREIGN KEY (expedition_id)
+    REFERENCES expedition(id)
+);
+
+INSERT INTO destination (
+  `name`,
+  `type`,
+  distance,
+  min_temperature,
+  max_temperature,
+  `image`
+  )
+
+VALUES
+  ('Mars', 'Planète', 56, -153, 20, 'mars.png'),
+  ('La Lune', 'Satellite', 0.384, -150, 150, 'moon.png'),
+  ('Venus', 'Planète', 40, 446, 490, 'venus.png');
